94c7d9bb17bf2da8c143a40a9d6d47376a5a8220
Steve Dakin <sdakin@adobe.com>
2014-03-05 23:07:08 -0800

    more game play logic

--- app/js/app/J7App.js
+++ app/js/app/J7App.js
@@ -18,7 +18,10 @@ define(
     function J7App() {
         EventTarget.call(this);
 
-        this.wheels = [0, 0, 0];
+        this.numWheels = 3;
+        this.wheels = [];
+        for (var i = 0 ; i < this.numWheels ; i++)
+            this.wheels.push({val:0, used:false});
     }
 
     J7App.prototype = new EventTarget();
@@ -30,6 +33,7 @@ define(
         var self = this;
 
         $("#btnSpin").click(function(e) { self.onSpin(); });
+        $("#btnNewGame").click(function(e) { self.onNewGame(); });
 
         self.onSpin();
     };
@@ -38,9 +42,42 @@ define(
         return $(".cell[data-val='" + value + "']");
     };
 
+    J7App.prototype.onNewGame = function(e) {
+        $(".cell").removeClass("playedcell");
+        var $gameControls = $("#controlArea").children();
+        $gameControls.last().hide();
+        $gameControls.first().show();
+        this.onSpin();
+    };
+
     J7App.prototype.onCellClick = function(e) {
+        var self = this;
+        var cellVal = parseInt($(e.target).text());
+
         $(e.target).addClass("playedcell");
         $(e.target).removeClass("validcell");
+
+        function checkCells(indices) {
+            var sum = 0, valid = true, checkSum = 0;
+            indices.forEach(function(index) {
+                valid &= !self.wheels[index].used;
+                checkSum += self.wheels[index].val;
+            });
+            if (valid && cellVal == checkSum) {
+                cellVal -= checkSum;
+                indices.forEach(function(index) {
+                    self.useWheel(index);
+                });
+            }
+        }
+
+        if (cellVal) {
+            checkCells([0]); checkCells([1]); checkCells([2]);
+            checkCells([0,1]); checkCells([0,2]); checkCells([1,2]);
+            checkCells([0,1,2]);
+        }
+
+        self.setValidCells();
     };
 
     J7App.prototype.onSpin = function() {
@@ -55,25 +92,65 @@ define(
 
     J7App.prototype.setValidCells = function() {
         var self = this;
-        $(".validcell").unbind("click");
+        $(".cell").unbind("click");
         $(".cell").removeClass("validcell");
-        self.getCell(self.wheels[0]).addClass("validcell");
-        self.getCell(self.wheels[1]).addClass("validcell");
-        self.getCell(self.wheels[2]).addClass("validcell");
-        self.getCell(self.wheels[0] + self.wheels[1]).addClass("validcell");
-        self.getCell(self.wheels[0] + self.wheels[2]).addClass("validcell");
-        self.getCell(self.wheels[1] + self.wheels[2]).addClass("validcell");
-        self.getCell(self.wheels[0] + self.wheels[1] + self.wheels[2]).addClass("validcell");
-        $(".validcell").click(function(e) { self.onCellClick(e); });
+
+        function checkWheels(indices) {
+            var valid = true, sum = 0;
+            indices.forEach(function(index) {
+                valid &= !self.wheels[index].used;
+                sum += self.wheels[index].val;
+            });
+            if (valid) {
+                var $cell = self.getCell(sum);
+                if (!$cell.hasClass("playedcell"))
+                    $cell.addClass("validcell");
+            }
+        }
+
+        checkWheels([0]); checkWheels([1]); checkWheels([2]);
+        checkWheels([0,1]); checkWheels([0,2]); checkWheels([1,2]);
+        checkWheels([0,1,2]);
+
+        var $validCells = $(".validcell");
+        if ($validCells.length == 0) {
+            var wheelsUsed = 0;
+            this.wheels.forEach(function(wheel) {
+                if (wheel.used)
+                    wheelsUsed++;
+            });
+
+            // the game is over if there are no valid cells and all wheels have not been used
+            if (wheelsUsed != this.numWheels) {
+                var $gameControls = $("#controlArea").children();
+                $gameControls.first().hide();
+                $gameControls.last().show();
+            } else {
+                self.onSpin();
+            }
+        } else {
+            $validCells.click(function(e) { self.onCellClick(e); });
+        }
     };
 
     J7App.prototype.setWheel = function(index, value) {
         var $wheels = $(".wheel");
         if (index >= 0 && index < $wheels.length) {
-            this.wheels[index] = value;
-            $($wheels[index]).text(value);
+            this.wheels[index] = {val:value, used:false};
+            var $wheel = $($wheels[index]);
+            $wheel.text(value);
+            $wheel.removeClass("usedwheel");
         }
     };
+
+    J7App.prototype.useWheel = function(index) {
+        var $wheels = $(".wheel");
+        if (index >= 0 && index < $wheels.length) {
+            var $wheel = $($wheels[index]);
+            $wheel.addClass("usedwheel");
+            this.wheels[index].used = true;
+        }
+    }
 
     return J7App;
 });
